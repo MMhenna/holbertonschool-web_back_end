fa9ed68e28d703c3627a730123f5378b81916b44
MMhenna <mhenna.mehdi@gmail.com>
2022-02-01 16:58:22 +0100

    commit

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,21 +7,17 @@
 
 from typing import List
 import asyncio
-import random
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''Execute task_wait_random and returns sorted list of delay'''
-    spawn_ls = []
-    delay_ls = []
+    delays: List[float] = []
+    all_delays: List[float] = []
     for i in range(n):
-        delayed_task = task_wait_random(max_delay)
-        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
-        spawn_ls.append(delayed_task)
-
-    for spawn in spawn_ls:
-        await spawn
-
-    return delay_ls
+        delays.append(task_wait_random(max_delay))
+    for delay in asyncio.as_completed(delays):
+        earliest_result = await delay
+        all_delays.append(earliest_result)
+    return all_delays
